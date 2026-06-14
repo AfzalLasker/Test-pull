c70be96366d8128b05886331019efc844bd52919
AfzalLasker <afzallasker98@gmail.com>
2026-06-14 19:23:33 -0400

    Week 4 assignment

--- testpull.cpp
+++ testpull.cpp
@@ -10,6 +10,12 @@
 
 // Include necessary libraries
 
+
+// Libraries used:
+// <iostream> - for input and output operations
+// <string> - for using string data type
+// <iomanip> - for formatting output (e.g., setw, setprecision)
+// <fstream> - for file handling (e.g., ofstream to save report to a
 #include <iostream>
 #include <string>
 #include <iomanip>
